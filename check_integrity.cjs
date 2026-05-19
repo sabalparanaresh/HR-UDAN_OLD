5b0d7fb379332661346cb752047d183d0bc877c3
sabalparanaresh <sabalparanaresh@gmail.com>
2026-05-19 18:35:17 +0530

    Delete check_integrity.cjs

--- check_integrity.cjs
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-try {
-  const db = new Database('statutory.db', { fileMustExist: true });
-  console.log("Integrity Check:", db.pragma('integrity_check', { simple: false }));
-} catch(e) {
-  console.error("error:", e);
-}
