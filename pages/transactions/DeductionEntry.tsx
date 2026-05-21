46afe3dea62ad424edc30bed0a30b69a0723bab7
sabalparanaresh <sabalparanaresh@gmail.com>
2026-05-21 13:40:45 +0530

    Delete pages directory

--- pages/transactions/DeductionEntry.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import VariableSalaryEntry from './VariableSalaryEntry';
-
-export default function DeductionEntry({ currentUser }: { currentUser: any }) {
-  return <VariableSalaryEntry type="DEDUCTION" currentUser={currentUser} />;
-}
--- pages/transactions/EarningEntry.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import VariableSalaryEntry from './VariableSalaryEntry';
-
-export default function EarningEntry({ currentUser }: { currentUser: any }) {
-  return <VariableSalaryEntry type="EARNING" currentUser={currentUser} />;
-}
